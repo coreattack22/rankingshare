549aa1245f06bbba21be00427564a028cc89793b
coreattack22 <coreattack22@gmail.com>
2018-07-10 19:18:05 +0900

    fix validation_error

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -259,25 +259,25 @@ class RankingsController < ApplicationController
 
     def ranking_params
       params.require(:ranking).permit(:maker_id, :title, :ranked_count, :answerd_at,
-      　　　　　　　　　:element1, :element2, :element3, :element4, :element5,
-      　　　　　　　　　:element6, :element7, :element8, :element9, :element10,
-      　　　　　　　　　:element11, :element12, :element13, :element14, :element15,
-      　　　　　　　　　:element16, :element17, :element18, :element19, :element20,
-      　　　　　　　　　:element21, :element22, :element23, :element24, :element25,
-      　　　　　　　　　:element26, :element27, :element28, :element29, :element30,
-      　　　　　　　　　:element31, :element32, :element33, :element34, :element35,
-      　　　　　　　　　:element36, :element37, :element38, :element39, :element40,
-      　　　　　　　　　:element41, :element42, :element43, :element44, :element45,
-      　　　　　　　　　:element46, :element47, :element48, :element49, :element50)
+      :element1, :element2, :element3, :element4, :element5,
+      :element6, :element7, :element8, :element9, :element10,
+      :element11, :element12, :element13, :element14, :element15,
+      :element16, :element17, :element18, :element19, :element20,
+      :element21, :element22, :element23, :element24, :element25,
+      :element26, :element27, :element28, :element29, :element30,
+      :element31, :element32, :element33, :element34, :element35,
+      :element36, :element37, :element38, :element39, :element40,
+      :element41, :element42, :element43, :element44, :element45,
+      :element46, :element47, :element48, :element49, :element50)
     end
 
     def answer_params
       params.require(:answer).permit(:ranking_id,:answerer_id,:comment,
-      　　　　　:answer1,:answer2,:answer3,:answer4,:answer5,:answer6,:answer7,:answer8,:answer9,:answer10,
-      　　　　　:answer11,:answer12,:answer13,:answer14,:answer15,:answer16,:answer17,:answer18,:answer19,:answer20,
-      　　　　　:answer21,:answer22,:answer23,:answer24,:answer25,:answer26,:answer27,:answer28,:answer29,:answer30,
-      　　　　　:answer31,:answer32,:answer33,:answer34,:answer35,:answer36,:answer37,:answer38,:answer39,:answer40,
-      　　　　　:answer41,:answer42,:answer43,:answer44,:answer45,:answer46,:answer47,:answer48,:answer49,:answer50)
+      :answer1,:answer2,:answer3,:answer4,:answer5,:answer6,:answer7,:answer8,:answer9,:answer10,
+      :answer11,:answer12,:answer13,:answer14,:answer15,:answer16,:answer17,:answer18,:answer19,:answer20,
+      :answer21,:answer22,:answer23,:answer24,:answer25,:answer26,:answer27,:answer28,:answer29,:answer30,
+      :answer31,:answer32,:answer33,:answer34,:answer35,:answer36,:answer37,:answer38,:answer39,:answer40,
+      :answer41,:answer42,:answer43,:answer44,:answer45,:answer46,:answer47,:answer48,:answer49,:answer50)
     end
 
 end
